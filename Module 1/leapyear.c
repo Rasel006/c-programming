4bee0c9b7e171ab2356bd1be9531ba9be79ad390
Rasel006 <you@example.com>
2023-04-03 19:42:19 +0600

    c programming

---
+++ Module 1/leapyear.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+int year;
+printf("Enter a year:");
+scanf("%d",&year);
+if(year%4==0)
+{
+    printf("it is a leap year.");
+}
+else{
+    printf("it is not a leap year");
+}
+
+}
